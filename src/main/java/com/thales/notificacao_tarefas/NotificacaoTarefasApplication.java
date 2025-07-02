a093dd4a85f8955173ba5364171106a95cfb7ea9
thales <thalesgarcezf@gmail.com>
2025-07-02 16:29:35 -0300

    Commit Inicial

---
+++ src/main/java/com/thales/notificacao_tarefas/NotificacaoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.thales.notificacao_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoTarefasApplication.class, args);
+	}
+
+}
